758cadf93946e8341bfee28c95506a91c31b5dcd
Sarah Majeed <sarahmajeed@Sarahs-MacBook-Air.local>
2022-01-18 16:48:36 +0500

    Adding query for bitcoin blockchain

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import "./App.css";
+
+interface Block {
+  __typename: string;
+  height: number;
+  transactionCount: number;
+}
+
+const BITCOIN_BLOCK = gql`
+  query data {
+    bitcoin {
+      blocks(options: { limit: 5, desc: "height" }) {
+        height
+        transactionCount
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(BITCOIN_BLOCK);
+  console.log(data.bitcoin.blocks);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {data.bitcoin.blocks.map((b: Block) => {
+          return (
+            <div>
+              <p>
+                transaction count of block number <b>{b.height}</b>:{" "}
+                {b.transactionCount}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// const client = new ApolloClient({
+//   uri: "https://graphql.bitquery.io",
+//   cache: new InMemoryCache(),
+// });
+
+const httpLink = createHttpLink({
+  uri: "https://graphql.bitquery.io",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = "BQYd1ev9RPQuIIektluqqkJzdSJZv3AM";
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": "BQYd1ev9RPQuIIektluqqkJzdSJZv3AM",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
